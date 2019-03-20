f7098a6130c631a9a7acc1a20a754cb83024fa73
thanhdolong <thanh.dolong@gmail.com>
2019-03-20 13:48:52 +0100

    Create a new Xcode project

---
+++ dopravaBrno/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dopravaBrno
+//
+//  Created by Thành Đỗ Long on 20/03/2019.
+//  Copyright © 2019 Thành Đỗ Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

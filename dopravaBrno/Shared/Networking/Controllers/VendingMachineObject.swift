be645227f50aa59972fbb496f06a57ab4dfb65d2
thanhdolong <thanh.dolong@gmail.com>
2019-04-02 16:31:07 +0200

    Edit vending machine object

--- dopravaBrno/Shared/Networking/Controllers/VendingMachineObject.swift
+++ dopravaBrno/Shared/Networking/Controllers/VendingMachineObject.swift
@@ -8,15 +8,34 @@
 
 import Foundation
 import Unbox
+import RealmSwift
 
-class VendingMachineObject: Unboxable {
-    public var location: Location;
+class VendingMachineObject: Object, Unboxable {
+    @objc dynamic var latitude: Double = 0.0
+    @objc dynamic var longitude: Double = 0.0
     
-    required init(unboxer: Unboxer) throws {
-        let latitude: Double = try unboxer.unbox(key: "lat")
-        let longitude: Double = try unboxer.unbox(key: "lon")
-        self.location = Location(name: "Automat na jizdenky", latitude: latitude, longitude: longitude)
+    required convenience init(unboxer: Unboxer) throws {
+        self.init()
+        
+        self.latitude = try unboxer.unbox(key: "lat")
+        self.longitude = try unboxer.unbox(key: "lon")
+//        self.location = Location(name: "Automat na jizdenky", latitude: latitude, longitude: longitude)
     }
+}
 
-
+extension VendingMachineObject {
+    convenience init(latitude: Double,
+                     longitude: Double) {
+        self.init()
+        
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    convenience init(vendingMachine: VendingMachine) {
+        self.init()
+        
+        self.latitude = vendingMachine.latitude
+        self.longitude = vendingMachine.longitude
+    }
 }

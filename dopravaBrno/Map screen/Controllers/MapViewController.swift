613d23c6a725f688c6ade6b80b2d9cb96e45a6b6
thanhdolong <thanh.dolong@gmail.com>
2019-04-02 16:53:07 +0200

    Remove unnecessary print

--- dopravaBrno/Map screen/Controllers/MapViewController.swift
+++ dopravaBrno/Map screen/Controllers/MapViewController.swift
@@ -21,7 +21,6 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hi")
         checkLocationServices()
         loadVendingMachines()
         addVendingMachinesToMap()

9b2a9d08f25894a0e079465cb810254a04af83e4
thanhdolong <thanh.dolong@gmail.com>
2019-04-06 20:10:52 +0200

    Add tab bar controller

---
+++ dopravaBrno/Application/Main/TabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarViewController.swift
+//  dopravaBrno
+//
+//  Created by Thành Đỗ Long on 06/04/2019.
+//  Copyright © 2019 Thành Đỗ Long. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // MARK: 1st VC
+        let firstViewController = UIViewController()
+        firstViewController.tabBarItem = UITabBarItem(title: "Red", image: nil, selectedImage: nil)
+        firstViewController.view.backgroundColor = .red
+
+        // Mark: 2nd VC
+        let secondViewController = MapViewController.storyboardInit()
+        secondViewController.tabBarItem = UITabBarItem(title: "Map", image: nil, selectedImage: nil)
+        
+        // Mark: 3rd VC
+        let thirdViewController = UIViewController()
+        thirdViewController.view.backgroundColor = .blue
+        thirdViewController.navigationItem.title = "Settings"
+        let thirdNavigationController = UINavigationController(rootViewController: thirdViewController)
+        thirdNavigationController.tabBarItem = UITabBarItem(title: "Settings", image: nil, selectedImage: nil)
+
+        self.viewControllers = [firstViewController, secondViewController, thirdNavigationController]
+        self.selectedViewController = secondViewController
+
+    }
+}

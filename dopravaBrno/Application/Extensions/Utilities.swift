23a1e53461580b0a22afa1a14cbfe974fc300a60
thanhdolong <thanh.dolong@gmail.com>
2019-04-07 23:08:12 +0200

    Hex color extension

--- dopravaBrno/Application/Extensions/Utilities.swift
+++ dopravaBrno/Application/Extensions/Utilities.swift
@@ -57,11 +57,40 @@ extension MKMapView {
 
 // MARK: Setup color with hex or RGB values extension
 extension UIColor {
+    convenience init(hexString: String) {
+        
+        let hexString: String = (hexString as NSString).trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner          = Scanner(string: hexString as String)
+        
+        if hexString.hasPrefix("#") {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:1)
+    }
+    
+    /**
+     Creates an UIColor Object based on provided RGB value in integer
+     - parameter red:   Red Value in integer
+     - parameter green: Green Value in integer
+     - parameter blue:  Blue Value in integer
+     - returns: UIColor with specified RGB
+     */
+    
     convenience init(red: Int, green: Int, blue: Int) {
         assert(red >= 0 && red <= 255, "Invalid red component")
         assert(green >= 0 && green <= 255, "Invalid green component")
         assert(blue >= 0 && blue <= 255, "Invalid blue component")
-        
         self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
     }
 }
@@ -94,6 +123,36 @@ extension UIApplication {
                 }
                 return
             }
+        }
+    }
+}
+
+extension UIView {
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
         }
     }
 }
